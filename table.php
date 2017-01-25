8455093f262806f7bac7722bea792e0b2e9522d5
Matthew Chau <nighton55infit@live.com>
2017-01-24 19:15:20 -0500

    Revert "Testing search function again"
    This reverts commit 90eb49921c1c9186f54327320d88263d06c75d26.

--- table.php
+++ table.php
@@ -14,25 +14,12 @@
 	<br></br>
 	
     <div id = "main">
-        <form action="table.php" method="post"/>
-			
-			<h2>Search Team Number:<h2>
-				<input type="int" name="search_input"/>
-			<br>
-			<center><input type="submit" value="Submit" /></center>
-		
-		</form>
-    </div>
-	
         <?php
             
             //Reusable connection to the sqlite database
             $handler = new PDO('sqlite:scout.db');
             $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			
-			//Sets search input to a variable (search)
-			$search = (int)$_POST['search_input'];
-			
+
             //Creates HTML table with headers for each column
             echo "<table>";
 
@@ -44,33 +31,20 @@
                 echo " </tr>";
             
                     //Sets $data to the data retrived by the defined query
-                    if (search != 0) {
-						$data = $handler->query("SELECT matchnum, team, score, scale FROM one WHERE teamnum = search");
-					}
-					else {
-						$data = $handler->query("SELECT matchnum, team, score, scale FROM one");
-                    }
-					
-					$data->setFetchMode(PDO::FETCH_ASSOC);
+                    $data = $handler->query("SELECT matchnum, team, score, scale FROM one");
+                    $data->setFetchMode(PDO::FETCH_ASSOC);
 
                 //Adds a table rows as needed and fills in table cells with data from database
-                
-			function populate() {
-				foreach($data as $row){
+                foreach($data as $row){
                     echo " <tr>";
                         foreach ($row as $name){
                             echo " <td>$name</td>";
                         }
                     echo " </tr>";
                 }
-			
-				echo "</table>";
-			}
-			
-			//Should re-execute function populate if submit button is pressed.
-			if(isset($_POST['submit'])) {
-				populate();
-			}
+
+            echo "</table>";
+
         ?>
     </div>
 
